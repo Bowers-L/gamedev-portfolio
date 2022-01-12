183e2381f14ba5542efe789f5322096f322c0b04
Bowers-L <loganmbowers@gmail.com>
2022-01-11 23:40:43 -0500

    Beatdown Stuff, also new images

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -18,6 +18,7 @@ export default [
     <li>Contribution: Project Lead, Level Designer, Programmer, VFX Artist</li>
     <li>Timeframe: Fall 2021</li>
     </ul>
+    </div>
 
     <div class="notice">
     Download my game <a href=https://drive.google.com/drive/folders/1fqnLaA5cIK2XJ06ff5WT-_P3rH9fmU5q target="_blank">here!</a>
@@ -60,21 +61,35 @@ export default [
     `, "#23bd69", true),
     new ProjectData("beatdown", "BeatDown", "img/projects/project-2-icon.png", `
     <div class="paragraph">
-        <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
+        <strong>BeatDown</strong> is a 2D rhythm game created in Spring 2021 as part of the VGDev Club at Georgia Tech. It has a unique game mechanic in that you are able to move
+        a player shape along a stage grid each time you hit the beat. The game features a charge mechanic that allows you to crush opponents, as well as powerups and ultimates. 
     </div>
+
+    <div class = "paragraph">
+    Project Info:
+
+    <ul>
+    <li>Genre: 2D Rhythm/Top Down</li>
+    <li>Engine: Unity/C#</li>
+    <li>Team Size: 13</li>
+    <li>Contribution: Programmer, Level Design</li>
+    <li>Timeframe: Spring 2021</li>
+    </ul>
+    </div>
+
     <div class="paragraph center">
         <iframe class="youtube" src="https://www.youtube.com/embed/8H40PtNLm68" frameborder="0" allowfullscreen></iframe>
     </div>
 
     <div class="paragraph">
-        Main features :
-        <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-        </ul>
+        My primary contribution to BeatDown was the design/implementation of the ultimate system. I wanted to design the system such that it incentivizes optimal gameplay while 
+        feeling balanced. The player gains an amount of ultimate charge by moving in conjunction with the beat, however, the player can also gain bonus charge by maintaining note streaks
+        and by killing opposing players. The player is also awarded more charge for "moving" rather than "charging". This incentivizes the player to move around the map more rather than waiting in
+        one corner. By giving each individual shape a different ultimate, the player is awarded a variety in playstyle based on their character choice. For example, the circle is incentivized to follow
+        other players closely since it's ultimate decimates players in its immediate vicinity, whereas the square wants to use powerups to limit other players' space and snipe them from farther away.
+        <br>
+        In terms of implementation, I took advantage of Unity's Scriptable Objects to easily tune values for balance purposes. I also used object oriented design to create separate coroutines for each ultimate
+        while providing an interface to the main player scripts. 
     </div>
 
     <div class="paragraph center">
@@ -109,10 +124,8 @@ export default [
     </div>
 
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
+        <img class="pc-screenshot" src="img/projects/AgentGunne/AgentGunneGameplay1.png" alt="Agent Gunne Gameplay" />
+        <img class="pc-screenshot" src="img/projects/AgentGunne/AgentGunneGameplay2.png" alt="Agent Gunne Gameplay" />
     </div>
     `, "#383838"),
     new ProjectData("project-4", "One Way Out", "img/projects/project-4-icon.png", `
@@ -160,14 +173,12 @@ export default [
 
     <div class="paragraph">
         <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        Doggy Disk is available to play on <a href="https://abnormal202.itch.io/doggy-disk" target="_blank">itch.io</a>.
         </div>
     </div>
 
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
+        <img class="pc-screenshot" src="img/projects/DoggyDisk/doggydisk2.webp" alt="Doggy Disk Gameplay" />
+        <img class="pc-screenshot" src="img/projects/DoggyDisk/doggydisk3.webp" alt="Doggy Disk Gameplay" />
     </div>`, "#e48246")
 ];
