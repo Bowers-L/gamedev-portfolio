f9f5159ead661589fe78a66e6a6139f08be07f04
Bowers-L <loganmbowers@gmail.com>
2022-03-05 15:49:12 -0500

    Updated home page to include slider (WIP)

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,7 +1,65 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("beam", "Beam", "img/projects/project-1-icon.png", 
+    new ProjectData("slider", "Slider", "img/projects/Slider-icon.png", 
+    `
+    <div class="paragraph">
+     <strong>Beam</strong> is a 3D first person puzzle game developed in Fall 2021 by the Georgia Tech VGDev club. I came up with the initial design and prototype, pitched the game to 
+     the club, and was the project lead during development. 
+    </div>
+
+    <div class = "paragraph">
+    Project Info:
+
+    <ul>
+    <li>Genre: 3D First Person Puzzle</li>
+    <li>Engine: Unity/C#</li>
+    <li>Team Size: 6</li>
+    <li>Contribution: Project Lead, Level Designer, Programmer, VFX Artist</li>
+    <li>Timeframe: Fall 2021</li>
+    </ul>
+    </div>
+
+    <div class="notice">
+    Play the game <a href=https://l-cpp.itch.io/beam target="_blank">here!</a>
+    </div>
+
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/Mg_dVNZKB9U" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        The primary puzzle mechanics of Beam are, of course, the Beams. The grab beam allows the user to control an object within line of sight. However, the catch is that the 
+        length of the beam must be constant while in use, meaning that the player can not contract or extend the beam. The swap beam then allows the user to swap places with an object.
+        <br>
+        I wanted to create an environment in which my team could come up with interesting level ideas and quickly iterate on the designs to create challenging, but fair levels. In order to 
+        accomplish this, I outlined a suggested pipeline for creating levels:
+        <ul>
+        <li>Concept Design- Come up with the general mechanics/concept that the level will test.</li>
+        <li>Mockup - Draw a top-down view of what the level with look like, including the solution.</li>
+        <li>Greybox - Block out the important level geometry in Unity (we used ProBuilder for this)</li>
+        <li>Playtest - Make sure the level is beatable. Look for alternative solutions.</li>
+        <li>Analyze/Modify - Make changes to the level based on feedback from playtesting. Do more playtesting/modifications if necessary.</li>
+        <li>Polish - Clean up the geometry. Add textures, lighting, narration, etc.</li>
+        </ul>
+        Playtesting during development was essential for gauging how difficult the puzzles were and how balanced the player felt compared to their environment. 
+        If the player's abilities were too powerful, then it would be hard to come up with intriguing puzzles without them feeling forced. 
+        For example, originally, objects would retain their momentum when the beam was released, however this became too powerful, so the momentum needed to be capped in order
+        to prevent players from throwing cubes across the map and immediately swapping to them. Another way to restrict the player's movement was to add special types of glass that prevented the player from using specific beam types.
+
+        <br>
+        Overall, I gained an intuitive understanding of what makes a game fun (specifically in the puzzle genre). It is important to create simple mechanics with a complex ceiling and to construct the environment in a way 
+        that restricts player choice just enough to allow for creative problem solving. The biggest challenge for the team and myself was definitely making levels that are the right amount of difficult while still being fun to solve.
+        Nonetheless, it was a rewarding experience, and I definitely learned a thing or two about design that I can use to make even better games in the future. 
+
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/Beam/BeamGameplay1.png" alt="Gameplay of Beam"/>
+        <img class="pc-screenshot" src="img/projects/Beam/BeamGameplay2.png" alt="Gameplay of Beam" />
+    </div>
+    `, "#23bd69", false, true), 
+    new ProjectData("beam", "Beam", "img/projects/Beam-icon.png", 
     `
     <div class="paragraph">
      <strong>Beam</strong> is a 3D first person puzzle game developed in Fall 2021 by the Georgia Tech VGDev club. I came up with the initial design and prototype, pitched the game to 
@@ -59,7 +117,7 @@ export default [
         <img class="pc-screenshot" src="img/projects/Beam/BeamGameplay2.png" alt="Gameplay of Beam" />
     </div>
     `, "#23bd69", true),
-    new ProjectData("beatdown", "BeatDown", "img/projects/project-2-icon.png", `
+    new ProjectData("beatdown", "BeatDown", "img/projects/Beatdown-icon.png", `
     <div class="paragraph">
         <strong>BeatDown</strong> is a 2D rhythm game created in Spring 2021 as part of the VGDev Club at Georgia Tech. It has a unique game mechanic in that you are able to move
         a player shape along a stage grid each time you hit the beat. The game features a charge mechanic that allows you to crush opponents, as well as powerups and ultimates. 
@@ -102,7 +160,7 @@ export default [
     <img class="pc-screenshot" src="img/projects/BeatDown/BeatDownGameplay2.webp" alt="BeatDown Gameplay" />
     </div>
     `, "#5a78af"),
-    new ProjectData("gunne monke", "Agent Gunne", "img/projects/project-3-icon.jpg", `
+    new ProjectData("gunne monke", "Agent Gunne", "img/projects/GunneMonke-icon.jpg", `
     <div class="paragraph">
         <strong>Agent Gunne</strong> (or <strong>Gunne Monke</strong>) is a game I worked on as part of the Game Design class (CS 4455) at Georgia Tech. 
         The game was designed and developed in the span of 6 weeks. I would consider this my first real experience creating a full 3D game with Unity, and it helped
@@ -147,7 +205,7 @@ export default [
         <img class="pc-screenshot" src="img/projects/AgentGunne/AgentGunneGameplay2.png" alt="Agent Gunne Gameplay" />
     </div>
     `, "#383838"),
-    new ProjectData("project-4", "One Way Out", "img/projects/project-4-icon.png", `
+    new ProjectData("project-4", "One Way Out", "img/projects/OneWayOut-icon.png", `
     <div class="paragraph">
     <strong>One Way Out</strong> was developed entirely in C and runs on the GBA. The game was playtested and 
     demoed through the Virtual Boy Advance - M (VBAM) emulator.
@@ -191,7 +249,7 @@ export default [
         provide in allowing developers to focus on design decisions over implementation details. 
     </div>
     `, "#e80fb7"),
-    new ProjectData("project-5", "Doggy Disk", "img/projects/project-5-icon.webp", `
+    new ProjectData("project-5", "Doggy Disk", "img/projects/DoggyDisk-icon.webp", `
     <div class="paragraph">
         <strong>Doggy Disk</strong> was developed in 48 hours as part of the Georgia Tech VGDev Summer Game Jam with the theme "All Around You".
     </div>
--- src/views/GameProjects.vue
+++ src/views/GameProjects.vue
@@ -3,11 +3,17 @@
 
     <div style="margin-bottom: 30px;">
         <h1>Hello!</h1> My name is <strong>Logan Bowers</strong>, a pupil of the programming arts and game developer. While I have engaged
-        in many fields of game development, my primary interests and experience are in gameplay design, level design, and gameplay systems programming. I strive
-        to create mechanics and situations that are easy for the player to understand, but have hidden depth in application. You can check out the games I've worked on below!
+        in many fields of game development, my primary interests and experience are in gameplay/graphics programming and game design.
+
+        You can check out the projects I've worked on below!
     </div>
 
     <ProjectsList v-bind:projects="projects" />
+
+    <h2>Programming Philosophy</h2>
+    My programming design philosophy is 3-fold: extensibility, maintainability, readability.<br>
+    I strive to create extendible solutions by using design principles such as <a href=https://www.digitalocean.com/community/conceptual_articles/s-o-l-i-d-the-first-five-principles-of-object-oriented-design>SOLID</a>
+    in conjunction with my own experience and judgment. I actively seek out and employ new strategies whenever possible to simplify solutions.
   </div>
 </template>
 
