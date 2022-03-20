c66e206122f3dcdbecd75570eafe71e2fae36780
Bowers-L <loganmbowers@gmail.com>
2022-03-20 19:51:08 -0400

    Fixed errors with new ProjectDaata

--- src/data/OtherProjectsData.ts
+++ src/data/OtherProjectsData.ts
@@ -25,7 +25,7 @@ export default [
           <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
           <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" /> 
         </div>
-    `, "#c10606", false, true),
+    `, "#c10606"),
     new ProjectData("project-7", "Drew Blood", "img/projects/project-7-icon.png", `
     <div class="paragraph">
                 <strong>Drew Blood</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
