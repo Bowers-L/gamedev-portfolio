62743ddf7c51083645c365e9e8e68f7e910f6ae4
L. Bowers <31969905+Bowers-L@users.noreply.github.com>
2023-10-01 21:41:41 -0400

    SkyLei Page

--- src/data/HomeProjectsData.ts
+++ src/data/HomeProjectsData.ts
@@ -1,12 +1,13 @@
 import ProjectData from '@/data/ProjectData.ts'
 
+import SkyLei from '@/data/Projects/SkyLei.ts'
 import Slider from '@/data/Projects/Slider.ts'
 import BloodFavor from '@/data/Projects/BloodFavor.ts'
 import Beam from '@/data/Projects/Beam.ts'
 
 
 export default [
-  BloodFavor,
+  SkyLei,
   Slider,
   Beam,
 ];
---
+++ src/data/Projects/SkyLei.ts
@@ -0,0 +1,15 @@
+import ProjectData from '@/data/ProjectData.ts'
+
+export default new ProjectData("skylei", "Sky Lei", "img/projects/SkyLei-logo.PNG", 
+`
+<div>
+Coming Soon ...
+<ul>
+<li>Genre: 3D Surreal/Exploratory Platformer</li>
+<li>Engine: Custom C++/OpenGL</li>
+<li>Team Size: 10+</li>
+<li>Contribution: Project Colead, Game Engine/Tools Programmer</li>
+<li>Timeframe: Fall 2023</li>
+</ul>
+</div>
+`, "#23bd69")
